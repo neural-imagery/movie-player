5c23097395d0323897aa4ac9ca7c731a2957386c
Raffi Hotter <raphael.hotter@gmail.com>
2023-09-21 19:25:44 -0400

    Add download csv button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player/youtube';
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player/youtube";
+import { CSVLink } from "react-csv";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 
 type TimestampEvent = {
   videoIndex: number;
   timestamp: string;
-  event: 'started' | 'ended' ;
+  event: "started" | "ended";
+  clipType: "action" | "disney" | "lecture";
+  clipUrl: string;
 };
 
 const Home: React.FC = () => {
@@ -12,67 +16,69 @@ const Home: React.FC = () => {
   const [currentClip, setCurrentClip] = useState<number>(0);
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
-  const clips: string[] = [
-    'https://www.youtube.com/watch?v=_46AoSnHCRo',
-    'https://www.youtube.com/watch?v=uoP3r22Ox_4',
+  const clips: { url: string; type: "action" | "disney" | "lecture" }[] = [
+    { url: "https://www.youtube.com/watch?v=_46AoSnHCRo", type: "action" },
+    { url: "https://www.youtube.com/watch?v=uoP3r22Ox_4", type: "disney" },
   ];
-  
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-  const recordTimestamp = (event: 'started' | 'ended' ) => {
+  const recordTimestamp = (event: "started" | "ended") => {
     const timestampEvent: TimestampEvent = {
       videoIndex: currentClip,
       timestamp: new Date().toLocaleString(),
       event,
+      clipType: clips[currentClip].type,
+      clipUrl: clips[currentClip].url,
     };
     setTimestamps([...timestamps, timestampEvent]);
   };
 
   const handleStart = () => {
-    if (timestamps[timestamps.length - 1]?.event !== 'started') {
-      recordTimestamp('started');
+    if (timestamps[timestamps.length - 1]?.event !== "started") {
+      recordTimestamp("started");
     }
   };
 
   const handleEnd = () => {
-    recordTimestamp('ended');
+    recordTimestamp("ended");
     if (currentClip < clips.length - 1) {
       setCurrentClip(currentClip + 1);
     }
   };
 
   return (
-    <div>
-      <h1>YouTube Clip Sequence</h1>
-
+    <div className="max-w-xl mx-auto mt-8">
       {isMounted && (
         <ReactPlayer
-          url={clips[currentClip]}
+          url={clips[currentClip].url}
           controls={true}
           onPlay={handleStart}
           onEnded={handleEnd}
         />
       )}
 
-      <h2>Timestamps</h2>
-      <ul>
-        {timestamps.map((timestampEvent, index) => (
-          <li key={index}>
-            Clip {timestampEvent.videoIndex + 1} {timestampEvent.event}: {timestampEvent.timestamp}
-          </li>
-        ))}
-      </ul>
+      <div className="mt-4">
+        <h2>Timestamps</h2>
+        <ul>
+          {timestamps.map((timestampEvent, index) => (
+            <li key={index}>
+              Clip {timestampEvent.videoIndex + 1} {timestampEvent.event}:{" "}
+              {timestampEvent.timestamp}
+            </li>
+          ))}
+        </ul>
+        <CSVLink data={timestamps} filename={"timestamps.csv"}>
+          <div className="flex items-center mt-4">
+            <ArrowDownTrayIcon className="h-5 w-5 mr-2" />
+            Download timestamps
+          </div>
+        </CSVLink>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
-
-
-
-// 'https://www.youtube.com/watch?v=_46AoSnHCRo',
-//     'https://www.youtube.com/watch?v=uoP3r22Ox_4',
